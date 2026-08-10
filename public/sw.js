2038530b5d29db41deda259a16e2423bb54b081c
ceciliapazyemilianovillagra-cpu <ceciliapazyemilianovillagra@gmail.com>
2026-08-10 10:03:37 -0300

    Agregar modo instalable y diseño adaptable

---
+++ public/sw.js
@@ -0,0 +1,2 @@
+const CACHE='progreso-plus-v1';const CORE=['/','/manifest.webmanifest','/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
+
